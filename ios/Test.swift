dfad301be3600a4d67be2a7315da0b195aaf72b1
苏依泉 <syq7970@outlook.com>
2018-01-30 14:51:53 +0800

    first commit

---
+++ ios/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  RNSwifterSwiftBugDemo
+//
+//  Created by 苏依泉 on 2018/1/30.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+import UIKit
+import SwifterSwift
+
+class Test: UIView {
+
+}
